617a1bbd0752c89aef9436d074d8af18ebf4b1cf
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-25 00:24:39 +0700

    chore(phase-1): set Android application id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.uts"
+    namespace = "com.falscode.ticketq"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,10 +31,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.uts"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.falscode.ticketq"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode

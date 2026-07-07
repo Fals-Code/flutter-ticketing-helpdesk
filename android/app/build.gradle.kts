bee5c64bad957a9bb88a57d8b3f9de86627abc21
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-07-08 01:20:46 +0700

    fix(release): sync launcher icons and production signing (#10)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,11 @@ android {
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = if (keystorePropertiesFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
